5a6efcd1f5901fcef17367a52783e3df10caa9d8
Viihtorugo <Victor-Torugo@hotmail.com>
2023-03-14 22:45:05 -0300

    Desenhando os comodos

--- README.md
+++ README.md
@@ -15,3 +15,16 @@ Executar o programa
 ```bash
  make all
 ```
+
+### Especificações da casa
+
++ 1 sala de estar - [30, 40] m²
++ 1 sala de jantar - [15, 30] m²
++ 1 cozinha - [10, 15] m²
++ 1 área de serviço - [6, 10] m²
++ 1 banheiro social - [3, 8] m²
++ 1 sala de ginástica - [20, 30] m²
++ 2 quartos com banheiro - [15, 38] m²
++ 1 quartos com closet - [15, 34] m²
++ **Obs.:** todos deve conter pelo menos uma porta e uma janela. Além de uma porta para área externa
++ **Obs.:** Intervalo da area [120, 240] m² 
--- app/main.c
+++ app/main.c
@@ -4,14 +4,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct hexa {
+typedef struct list {
   int x, y;
-  struct hexa *next;
-} hexa;
+  struct list *next;
+} list;
 
-hexa *add_vertex (int x, int y, hexa *h)
+list *add_vertex (int x, int y, list *head)
 {
-  hexa *new_vertex = (hexa *) malloc(sizeof(hexa));
+  list *new_vertex = (list *) malloc(sizeof(list));
 
   if (new_vertex == NULL)
   {
@@ -21,32 +21,35 @@ hexa *add_vertex (int x, int y, hexa *h)
 
   new_vertex->x = x;
   new_vertex->y = y;
-
-  if (h == NULL)
+  new_vertex->next = NULL;
+  
+  if (head == NULL)
   {
     return new_vertex;
   }
   else
   {
-    hexa *aux = h;
+    list *aux = head;
 
     while (aux->next != NULL)
       aux = aux->next;
     
     aux->next = new_vertex;
 
-    return h;
+    return head;
   }
 }
 
-hexa *create_hexa_for_area (int area, int pos)
+
+
+list *build_hexa_for_area (int area, int pos)
 {
-  hexa *h = NULL;
+  list *head = NULL;
 
   if (pos == 1)
   {
-    int x0 = 400, x1 = 400, x2 = 400, x3 = 400, x4 = 400, x5 = 400;
-    int y0 = 400 + (area/1000), y1 = 400, y2 = 400, y3 = 400 - (area/1000), y4 = 400, y5 = 400;
+    int x0 = 450, x1 = 450, x2 = 450, x3 = 450, x4 = 450, x5 = 450;
+    int y0 = 450 + (area/1500), y1 = 450, y2 = 450, y3 = 450 - (area/1500), y4 = 450, y5 = 450;
 
     int d1 = ((x1*y2 + x2*y3 + x2*y4 + x4*y1) - (x1*y2 + x3*y2 + x4*y3 + x1*y4));
     int d2 = ((x0*y1 + x1*y5 + x5*y0) - (x1*y0 + x5*y1 + x0*y5));
@@ -94,10 +97,7 @@ hexa *create_hexa_for_area (int area, int pos)
       {
         d1 = aux;
       }
-      
 
-     // printf("d1 = %d\n", d1);
-  
       i += 1;
     }
     
@@ -109,28 +109,14 @@ hexa *create_hexa_for_area (int area, int pos)
       printf("(x4, y4) = (%d,%d)\n", x4, y4);
       printf("(x5, y5) = (%d,%d)\n", x5, y5);
 
-      /*
-      d2 = ((x0*y1 + x1*y5 + x5*y0) - (x1*y0 + x5*y1 + x0*y5));
-      
-      if (d2 < 0)
-        d2 *= (-1);
-      
-      d3 = ((x3*y2 + x2*y4 + x4*y3) - (x2*y3 + x4*y2 + x3*y4));
+      head = add_vertex(x0, y0, head);
+      head = add_vertex(x1, y1, head);
+      head = add_vertex(x2, y2, head);
+      head = add_vertex(x3, y3, head);
+      head = add_vertex(x4, y4, head);
+      head = add_vertex(x5, y5, head);
+      head = add_vertex(x0, y0, head);
 
-      if (d3 < 0)
-        d3 *= (-1);
-      */
-
-      h = add_vertex(x0, y0, h);
-      h = add_vertex(x1, y1, h);
-      h = add_vertex(x2, y2, h);
-      h = add_vertex(x3, y3, h);
-      h = add_vertex(x4, y4, h);
-      h = add_vertex(x5, y5, h);
-
-      return h;
-
-    printf("test\n");
   }
   else if (pos == 2)
   {
@@ -145,7 +131,201 @@ hexa *create_hexa_for_area (int area, int pos)
     printf("test\n");
   }
 
-  return h;
+  return head;
+}
+
+list *build_bedroom_with_bathroom (int x0, int y0, int area)
+{
+  list *head = NULL;
+
+  int x1 = x0, x2 = x0, x3 = x0;
+  int y1 = y0, y2 = y0, y3 = y0;
+  
+  int area_bedroom = (x0*y1 + x1*y2 + x2*y3 + x3*y0) - (x1*y0 + x2*y1 + x3*y2 + x0*y3);
+
+  printf("area = %d\n", area);
+
+  while (area_bedroom < area)
+  {
+    y1 -= 1;
+    x3 -= 1;
+    x2 -= 1;
+    y2 -= 1;
+
+    area_bedroom = (x0*y1 + x1*y2 + x2*y3 + x3*y0) - (x1*y0 + x2*y1 + x3*y2 + x0*y3);
+
+    if (area_bedroom < 0)
+      area_bedroom *= (-1); 
+  }
+  
+  printf("area = %d\n", area_bedroom);
+  printf("\nquarto\n\n");
+  printf("(x0, y0) = (%d,%d)\n", x0, y0);
+  printf("(x1, y1) = (%d,%d)\n", x1, y1);
+  printf("(x2, y2) = (%d,%d)\n", x2, y2);
+  printf("(x3, y3) = (%d,%d)\n", x3, y3);
+
+  head = add_vertex(x0, y0, head);
+  head = add_vertex(x1, y1, head);
+  head = add_vertex(x2, y2, head);
+  head = add_vertex(x3, y3, head);
+  head = add_vertex(x0, y0, head);
+ 
+  return head;
+}
+
+list *build_gym_room (int x0, int x1, int y0, int area)
+{
+  list *head = NULL;
+
+  int x2 = x1, x3 = x0;
+  int y1 = y0, y2 = y0, y3 = y0;
+  
+  int area_gym_room = (x0*(y1 + y0)+ x1*(y2 + y3)) - (x1*(y1 + y0)+ x0*(y2 + y3));
+
+  while (area_gym_room < area)
+  {
+    y2 -= 1;
+    y3 -= 1;
+    
+    area_gym_room = (x0*(y1 + y0) + x1*(y2 + y3)) - (x1*(y1 + y0)+ x0*(y2 + y3));
+
+    if (area_gym_room < 0)
+      area_gym_room *= (-1); 
+    
+  }
+  
+  printf("area = %d\n", area_gym_room);
+  printf("\nsala de ginastica\n\n");
+  printf("(x0, y0) = (%d,%d)\n", x0, y0);
+  printf("(x1, y1) = (%d,%d)\n", x1, y1);
+  printf("(x2, y2) = (%d,%d)\n", x2, y2);
+  printf("(x3, y3) = (%d,%d)\n", x3, y3);
+
+  head = add_vertex(x0, y0, head);
+  head = add_vertex(x1, y1, head);
+  head = add_vertex(x2, y2, head);
+  head = add_vertex(x3, y3, head);
+  head = add_vertex(x0, y0, head);
+ 
+  return head;
+}
+
+list *build_room (int x0, int y0, int area)
+{
+   list *head = NULL;
+
+  int x1 = x0, x2 = x0, x3 = x0;
+  int y1 = y0, y2 = y0, y3 = y0;
+  
+  int area_room = (x0*y1 + x1*y2 + x2*y3 + x3*y0) - (x1*y0 + x2*y1 + x3*y2 + x0*y3);
+
+  while (area_room < area)
+  {
+    y1 += 1;
+    y2 += 1;
+    x2 += 1;
+    x3 += 1;
+    
+    area_room = (x0*y1 + x1*y2 + x2*y3 + x3*y0) - (x1*y0 + x2*y1 + x3*y2 + x0*y3);
+
+    if (area_room < 0)
+      area_room *= (-1); 
+    
+  }
+  
+  printf("area = %d\n", area_room);
+  printf("\nsala de estar\n\n");
+  printf("(x0, y0) = (%d,%d)\n", x0, y0);
+  printf("(x1, y1) = (%d,%d)\n", x1, y1);
+  printf("(x2, y2) = (%d,%d)\n", x2, y2);
+  printf("(x3, y3) = (%d,%d)\n", x3, y3);
+
+  head = add_vertex(x0, y0, head);
+  head = add_vertex(x1, y1, head);
+  head = add_vertex(x2, y2, head);
+  head = add_vertex(x3, y3, head);
+  head = add_vertex(x0, y0, head);
+ 
+  return head;
+}
+
+list *build_dining_room (int x0, int y0, int area)
+{
+   list *head = NULL;
+
+  int x1 = x0, x2 = x0, x3 = x0;
+  int y1 = y0, y2 = y0, y3 = y0;
+  
+  int dining_room = (x0*y1 + x1*y2 + x2*y3 + x3*y0) - (x1*y0 + x2*y1 + x3*y2 + x0*y3);
+
+  while (dining_room < area)
+  {
+    x1 -= 1;
+    y2 += 1;
+    x2 -= 1;
+    y3 += 1;
+    
+    dining_room = (x0*y1 + x1*y2 + x2*y3 + x3*y0) - (x1*y0 + x2*y1 + x3*y2 + x0*y3);
+
+    if (dining_room < 0)
+      dining_room *= (-1); 
+    
+  }
+  
+  printf("area = %d\n", dining_room);
+  printf("\nsala de jantar\n\n");
+  printf("(x0, y0) = (%d,%d)\n", x0, y0);
+  printf("(x1, y1) = (%d,%d)\n", x1, y1);
+  printf("(x2, y2) = (%d,%d)\n", x2, y2);
+  printf("(x3, y3) = (%d,%d)\n", x3, y3);
+
+  head = add_vertex(x0, y0, head);
+  head = add_vertex(x1, y1, head);
+  head = add_vertex(x2, y2, head);
+  head = add_vertex(x3, y3, head);
+  head = add_vertex(x0, y0, head);
+ 
+  return head;
+}
+
+list *build_kitchen (int x0, int y0, int area)
+{
+   list *head = NULL;
+
+  int x1 = x0, x2 = x0, x3 = x0;
+  int y1 = y0, y2 = y0, y3 = y0;
+  
+  int area_kitchen = (x0*y1 + x1*y2 + x2*y3 + x3*y0) - (x1*y0 + x2*y1 + x3*y2 + x0*y3);
+
+  while (area_kitchen < area)
+  {
+    x1 -= 1;
+    y2 += 1;
+    x2 -= 1;
+    y3 += 1;
+    
+    area_kitchen = (x0*y1 + x1*y2 + x2*y3 + x3*y0) - (x1*y0 + x2*y1 + x3*y2 + x0*y3);
+
+    if (area_kitchen < 0)
+      area_kitchen *= (-1); 
+    
+  }
+  
+  printf("area = %d\n", area_kitchen);
+  printf("\ncozinha\n\n");
+  printf("(x0, y0) = (%d,%d)\n", x0, y0);
+  printf("(x1, y1) = (%d,%d)\n", x1, y1);
+  printf("(x2, y2) = (%d,%d)\n", x2, y2);
+  printf("(x3, y3) = (%d,%d)\n", x3, y3);
+
+  head = add_vertex(x0, y0, head);
+  head = add_vertex(x1, y1, head);
+  head = add_vertex(x2, y2, head);
+  head = add_vertex(x3, y3, head);
+  head = add_vertex(x0, y0, head);
+ 
+  return head;
 }
 
 void display()
@@ -153,28 +333,6 @@ void display()
   glClear(GL_COLOR_BUFFER_BIT);
 
   glBegin(GL_LINES);
-
-  //linha 1
-  //glVertex2i(400, 600);
-  //glVertex2i(400, 800);
-  //linha 2
-  glVertex2i(400, 20);
-  glVertex2i(400, 800);
-  glVertex2i(10, 400);
-  glVertex2i(800, 400);
-  /*linha 3
-  glVertex2i(400, 800);
-  glVertex2i(500, 900);
-  //linha 4
-  glVertex2i(500, 900);
-  glVertex2i(600, 800);
-  //linha 5
-  glVertex2i(400, 600);
-  glVertex2i(500, 500);
-  //linha 6
-  glVertex2i(600, 600);
-  glVertex2i(500, 500);
-  */
 
   glEnd();
   glFlush();
@@ -186,71 +344,157 @@ int main(int argc, char **argv)
 
   while (1)
   {
-    printf("Digite a area da casa (area >= 50000 || area <= 200000): ");
+    printf("Digite a area da casa (area >= 135000 && area <= 255000): ");
     scanf("%d", &area);
     
-    if(area >= 50000 && area <= 2000000)
-    {
+    if(area >= 130000 && area <= 255000)
       break;
-    }
 
     printf("Valor da area nao esta no intervalo.\n\n");
   }
 
-  hexa *h = create_hexa_for_area(area, 1);
+  list *hexa = build_hexa_for_area(area, 1);
   
   glutInit(&argc, argv);
   glutInitDisplayMode(GLUT_SINGLE);
-  glutInitWindowSize(820, 820);
-  glutInitWindowPosition(100, 100);
+  glutInitWindowSize(920, 920);
+  glutInitWindowPosition(100, 0);
   glutCreateWindow("Planta baixa de casa");
-  gluOrtho2D(0.0, 820.0, 0.0, 820.0);
+  gluOrtho2D(0.0, 920.0, 0.0, 920.0);
   //glutDisplayFunc(display);
 
   glClear(GL_COLOR_BUFFER_BIT);
 
-  glBegin(GL_LINES);
+  glBegin(GL_LINE_STRIP);
 
-  //linha 1
-  //glVertex2i(400, 600);
-  //glVertex2i(400, 800);
-  //linha 2
-  glVertex2i(400, 20);
-  glVertex2i(400, 800);
-  glVertex2i(20, 400);
-  glVertex2i(800, 400);
-  /*linha 3
-  glVertex2i(400, 800);
-  glVertex2i(500, 900);
-  //linha 4
-  glVertex2i(500, 900);
-  glVertex2i(600, 800);
-  //linha 5
-  glVertex2i(400, 600);
-  glVertex2i(500, 500);
-  //linha 6
-  glVertex2i(600, 600);
-  glVertex2i(500, 500);
-  */
+  list *aux = hexa;
+  list *room = NULL;
+  list *kitchen = NULL;
 
-  hexa *aux = h;
+  int i = 0;
 
   while (aux != NULL)
   {
-    if (aux->next == NULL)
-    {
-      glVertex2i(aux->x, aux->y);
-      glVertex2i(h->x, h->y);
-    }
-    else
-    {
-      glVertex2i(aux->x, aux->y);
-      glVertex2i(aux->next->x, aux->next->y);
-    }
+    glVertex2i(aux->x, aux->y);
+    
+    if (i == 2)
+      kitchen = aux;
 
+    if (i == 4)
+      room = aux;
+
+    i += 1;
     aux = aux->next;
   }
   
+  list *q1 = build_bedroom_with_bathroom (hexa->next->x, hexa->next->y, 15000);
+  list *prev = NULL;
+  list *v1 = q1->next;
+  
+  while (q1 != NULL)
+  {
+    glVertex2i(q1->x, q1->y);
+    
+    if (q1->next != NULL)
+    {
+      prev = q1;
+    }
+    
+    q1 = q1->next;
+  }
+  
+
+  list *q2 = build_bedroom_with_bathroom (prev->x, prev->y, 15000);
+
+  while (q2 != NULL)
+  {
+    if (q2->next != NULL)
+    {
+      prev = q2;
+    }
+
+    glVertex2i(q2->x, q2->y);
+    q2 = q2->next;
+  }
+
+  list *q3 = build_bedroom_with_bathroom (prev->x, prev->y, 15000);
+  list *v2 = q3->next;
+
+  while (q3 != NULL)
+  {
+    if (q3->next != NULL)
+    {
+      prev = q3;
+    }
+
+    glVertex2i(q3->x, q3->y);
+    q3 = q3->next;
+  }
+
+  glEnd();
+  glFlush();
+
+  glBegin(GL_LINE_STRIP);
+
+  list *gym_room = build_gym_room (v1->x, v2->x, v1->y, 20000);
+
+  while (gym_room != NULL)
+  {
+    if (gym_room->next != NULL)
+    {
+      prev = gym_room;
+    }
+
+    glVertex2i(gym_room->x, gym_room->y);
+    gym_room = gym_room->next;
+  }
+
+  glEnd();
+  glFlush();
+
+  glBegin(GL_LINE_STRIP);
+
+  room = build_room (room->x, room->y, 30000);
+
+  while (room != NULL)
+  {
+    glVertex2i(room->x, room->y);
+    room = room->next;
+  }
+
+  glEnd();
+  glFlush();
+
+  glBegin(GL_LINE_STRIP);
+
+  kitchen = build_kitchen (kitchen->x, kitchen->y, 10000);
+  list *dining_room = NULL;
+  i = 0;
+
+  while (kitchen != NULL)
+  {
+    glVertex2i(kitchen->x, kitchen->y);
+
+    if (i == 3)
+      dining_room = kitchen;
+
+
+    i += 1;
+    kitchen = kitchen->next;
+  }
+
+  glEnd();
+  glFlush();
+
+  glBegin(GL_LINE_STRIP);
+
+  dining_room = build_dining_room (dining_room->x, dining_room->y, 15000);
+
+  while (dining_room != NULL)
+  {
+    glVertex2i(dining_room->x, dining_room->y);
+    dining_room = dining_room->next;
+  }
 
   glEnd();
   glFlush();
